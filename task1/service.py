430d5338e9e4b44765318fa0cc5d94d5b269701c
Maksim <makc.pinsk@gmail.com>
2021-02-19 22:20:47 +0300

    fix name file on path_file

--- task1/service.py
+++ task1/service.py
@@ -6,30 +6,30 @@ def check_outfile(outfile):
     """ Check outfile file on right format"""
 
     format_outfile = os.path.splitext(outfile)[1]
-    if format_outfile  in ['.csv', '.parquet']:
+    if format_outfile in ['.csv', '.parquet']:
         return True
     else:
         return False
 
 
-def check_file(file, outfile):
+def check_file(path_file, outfile):
     """ Check input file file on right format"""
 
-    name_file = os.path.basename(file)
+    name_file = os.path.basename(path_file)
     format_file = os.path.splitext(name_file)[1]
     if format_file == '.csv':
-        return parsing_csv_in_parquet(file, outfile)
+        return parsing_csv_in_parquet(path_file, outfile)
     elif format_file == '.parquet':
-        return parsing_parquet_in_csv(file, outfile)
+        return parsing_parquet_in_csv(path_file, outfile)
     else:
         return 'Format file not csv or parquet'
 
 
-def check_schema(file):
+def check_schema(path_file):
     """ Check parquet file on right format"""
 
-    format_schema = os.path.splitext(file)[-1]
+    format_schema = os.path.splitext(path_file)[-1]
     if format_schema == '.parquet':
-        return get_parquet_schema(file)
+        return get_parquet_schema(path_file)
     else:
         return 'File not parquet format'
--- task1/view.py
+++ task1/view.py
@@ -11,39 +11,39 @@ def find_name_file(path_file):
     return name_file
 
 
-def parsing_csv_in_parquet(file, outfile):
+def parsing_csv_in_parquet(path_file, outfile):
     """ Parsing csv file in parquet file """
 
     try:
-        file_csv = pd.read_csv(file, index_col=False, header=0)
+        file_csv = pd.read_csv(path_file, index_col=False, header=0)
         table = Table.from_pandas(file_csv, preserve_index=True)
         if not outfile:
-            outfile = find_name_file(file) + '.parquet'
+            outfile = find_name_file(path_file) + '.parquet'
         parquet.write_table(table, outfile)
         return 'File: ' + outfile
     except Exception as e:
         return e
 
 
-def parsing_parquet_in_csv(file, outfile):
+def parsing_parquet_in_csv(path_file, outfile):
     """ Parsing parquet file in csv file"""
 
     try:
 
-        file_parquet = parquet.read_table(file)
+        file_parquet = parquet.read_table(path_file)
         table = file_parquet.to_pandas()
         if not outfile:
-            outfile = find_name_file(file) + '.csv'
+            outfile = find_name_file(path_file) + '.csv'
         table.to_csv(outfile, index=False)
         return 'File: ' + outfile
     except BaseException as e:
         return 'Error:', e
 
 
-def get_parquet_schema(file):
+def get_parquet_schema(path_file):
     """ Get schema from parquet file"""
     try:
-        parquet_schema = parquet.ParquetFile(file)
+        parquet_schema = parquet.ParquetFile(path_file)
         return parquet_schema.schema
     except Exception as e:
         return e
