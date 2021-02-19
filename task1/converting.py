7c36c434e179b172175c0e5622821410228613a5
Maksim <makc.pinsk@gmail.com>
2021-02-19 23:53:45 +0300

    fix bags

--- task1/converting.py
+++ task1/converting.py
@@ -3,47 +3,45 @@ import pandas as pd
 from pyarrow import parquet, Table
 
 
-def find_name_file(path_file):
+def find_name_file(file_path):
     """Return name file which is being processed """
 
-    file = os.path.basename(path_file)
+    file = os.path.basename(file_path)
     name_file = os.path.splitext(file)[0]
     return name_file
 
 
-def parsing_csv_in_parquet(path_file, outfile):
+def parsing_csv_in_parquet(file_path, outfile):
     """ Parsing csv file in parquet file """
 
     try:
-        file_csv = pd.read_csv(path_file, index_col=False, header=0)
+        file_csv = pd.read_csv(file_path, index_col=False, header=0)
         table = Table.from_pandas(file_csv, preserve_index=True)
         if not outfile:
-            outfile = find_name_file(path_file) + '.parquet'
+            outfile = find_name_file(file_path) + '.parquet'
         parquet.write_table(table, outfile)
-        return 'File: ' + outfile
     except Exception as e:
         raise e
 
 
-def parsing_parquet_in_csv(path_file, outfile):
+def parsing_parquet_in_csv(file_path, outfile):
     """ Parsing parquet file in csv file"""
 
     try:
 
-        file_parquet = parquet.read_table(path_file)
+        file_parquet = parquet.read_table(file_path)
         table = file_parquet.to_pandas()
         if not outfile:
-            outfile = find_name_file(path_file) + '.csv'
+            outfile = find_name_file(file_path) + '.csv'
         table.to_csv(outfile, index=False)
-        return 'File: ' + outfile
     except BaseException as e:
         raise e
 
 
-def get_parquet_schema(path_file):
+def get_parquet_schema(file_path):
     """ Get schema from parquet file"""
     try:
-        parquet_schema = parquet.ParquetFile(path_file)
+        parquet_schema = parquet.ParquetFile(file_path)
         return parquet_schema.schema
     except Exception as e:
         raise e
--- task1/main.py
+++ task1/main.py
@@ -1,9 +1,10 @@
 import sys
 import argparse
-from service import check_outfile, check_schema, check_file
+import os
+from converting import parsing_parquet_in_csv, get_parquet_schema, parsing_csv_in_parquet
 
 
-def createParser():
+def get_parse_arguments():
     """Create params command line"""
 
     parser = argparse.ArgumentParser()
@@ -12,21 +13,37 @@ def createParser():
     parser.add_argument('-s', '--schema')
 
     return parser
+def check_outfile(outfile):
+    """ Check outfile file on right format"""
 
+    format_outfile = os.path.splitext(outfile)[1]
+    return format_outfile in ['.csv', '.parquet']
 
 def main():
     """Processing the command line and calling functions for the parsing file """
 
-    parser = createParser()
+    parser = get_parse_arguments()
     namespace = parser.parse_args(sys.argv[1:])  # 1-й параметр - расположение файла
     if namespace.outfile:
         if not check_outfile(namespace.outfile):
             return 'Outfile not csv or parquet format'
     if namespace.file:
-        return check_file(namespace.file, namespace.outfile)
+        name_file = os.path.basename(namespace.file)
+        format_file = os.path.splitext(name_file)[1]
+        if format_file == '.csv':
+            return parsing_csv_in_parquet(namespace.file, namespace.outfile)
+        elif format_file == '.parquet':
+            return parsing_parquet_in_csv(namespace.file, namespace.outfile)
+        else:
+            return 'Format file not csv or parquet'
+
     if namespace.schema:
-        return check_schema(namespace.schema)
+        format_schema = os.path.splitext(namespace.schema)[-1]
+        if format_schema == '.parquet':
+            return get_parquet_schema(namespace.schema)
+        else:
+            return 'File not parquet format'
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
