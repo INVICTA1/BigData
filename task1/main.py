0239c799fafc941dd93ac72eca5069438cf3d998
Maksim <makc.pinsk@gmail.com>
2021-02-19 18:19:22 +0300

    input data from console

--- task1/main.py
+++ task1/main.py
@@ -1,61 +1,89 @@
 import os
 import pandas as pd
 from pyarrow import parquet, Table
-
-# csv_path_file = 'resources/csv/userdata1.csv'
-# parquet_path_file = 'resources/parquet/userdata5.parquet'
-
-parquet_folder = 'resources/parquet/parsing-csv-files/'
-csv_folder = 'resources/csv/parsing-parquet-files/'
+import sys
+import argparse
 
 
 def find_name_file(path_file):
+    """Return name file which is being processed """
+
     file = os.path.basename(path_file)
     name_file = os.path.splitext(file)[0]
     return name_file
 
 
-def parsing_csv_in_parquet():
+def parsing_csv_in_parquet(file, outfile):
     """ Parsing csv file in parquet file """
-    path_file = input('Enter path to file: ')
-    file_csv = pd.read_csv(path_file, index_col=False)
-    parquet_file = parquet_folder + find_name_file(path_file) + '.parquet'
-    table = Table.from_pandas(file_csv, preserve_index=False)
-    parquet.write_table(table, parquet_file)
-    return 'File: ' + parquet_file
+
+    try:
+        file_csv = pd.read_csv(file, index_col=False, header=0)
+        table = Table.from_pandas(file_csv, preserve_index=False)
+        if not outfile:
+            outfile = find_name_file(file) + '.parquet'
+        parquet.write_table(table, outfile)
+        return 'File: ' + outfile
+    except Exception as e:
+        return e
 
 
-def parsing_parquet_in_csv():
+def parsing_parquet_in_csv(file, outfile):
     """ Parsing parquet file in csv file"""
-    path_file = input('Enter path to file: ')
-    file_parquet = parquet.read_table(path_file)
-    csv_file = csv_folder + find_name_file(path_file) + '.csv'
-    table = file_parquet.to_pandas()
-    table.to_csv(csv_file)
-    return 'File: ' + csv_file
+
+    try:
+
+        file_parquet = parquet.read_table(file)
+        table = file_parquet.to_pandas()
+        if not outfile:
+            outfile = find_name_file(file) + '.csv'
+        table.to_csv(outfile)
+        return 'File: ' + outfile
+    except BaseException as e:
+        return 'Error:', e
 
 
-def get_parquet_schema():
+def get_parquet_schema(file):
     """ Get schema from parquet file"""
-    path_file = input('Enter path to file: ')
-    parquet_schema = parquet.ParquetFile(path_file)
-    return parquet_schema.schema
+    try:
+        parquet_schema = parquet.ParquetFile(file)
+        return parquet_schema.schema
+    except Exception as e:
+        return e
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file')
+    parser.add_argument('-o', '--outfile', default=None)
+    parser.add_argument('-s', '--schema')
+
+    return parser
 
 
 def main():
-    methods = {1: parsing_csv_in_parquet,
-               2: parsing_parquet_in_csv,
-               3: get_parquet_schema}
-    while True:
-        try:
-            num = int(input('1 : parsing csv in parquet\n'
-                            '2 : parsing parquet in csv\n'
-                            '3 : get parquet schema\n'
-                            'Exit : any symbol'
-                            'Enter number: '))
-            print(methods[num]())
-        except:
-            break
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])  # 1-й параметр - расположение файла
+    if namespace.outfile:
+        format_outfile = os.path.splitext(namespace.outfile)[1]
+        if format_outfile not in ['.csv', '.parquet']:
+            return 'Outfile not csv or parquet format'
+    elif namespace.file:
+        file = os.path.basename(namespace.file)
+        format_file = os.path.splitext(file)[1]
+        if format_file == '.csv':
+            return parsing_csv_in_parquet(namespace.file, namespace.outfile)
+        elif format_file == '.parquet':
+            return parsing_parquet_in_csv(namespace.file, namespace.outfile)
+        else:
+            return 'Format file not csv or parquet'
 
-if __name__=='__main__':
-    main()
+    if namespace.schema:
+        format_schema = os.path.splitext(namespace.outfile)[1]
+        if format_schema == '.parquet':
+            return get_parquet_schema(namespace.schema)
+        else:
+            return 'File not parquet format'
+
+
+if __name__ == '__main__':
+    print(main())
